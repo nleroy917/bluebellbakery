15177dbe6213a92aae88426ff2f869a229cf90de
Nathan LeRoy <NLeRoy917@gmail.com>
2024-07-31 12:43:40 -0400

    remove things

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/bluebellbakery',
-  images: {
-    unoptimized: true,
-  },
 }
 
 export default nextConfig
